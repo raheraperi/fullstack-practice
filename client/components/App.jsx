9eef47d4d6b5b70f93bacd99f2dce59ced8c1e32
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-08-13 16:52:24 +1200

    app.jsx

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Route, Link} from 'react-router-dom'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        hi hi
+        <Link />
+        <Route />
+      </div>
+    )
+  }
+}
+
+export default App
